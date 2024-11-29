e495987736aed077f082b988ff38bc94f00a4fc5
Victor Freebyz <victor.m@freebyz.com>
2024-11-29 13:43:49 +0100

    Exceptions file structure done

--- app/Exceptions/ForbiddenException.php
+++ app/Exceptions/ForbiddenException.php
@@ -3,8 +3,25 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Response;
 
 class ForbiddenException extends Exception
 {
-    //
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        $response = [
+            'status' => false,
+            'message' => $this->getMessage() ?: 'Forbidden',
+            'errors' => $this->getCode() ? $this->getCode() : []
+        ];
+
+        return response()->json($response, Response::HTTP_FORBIDDEN);
+    }
 }
--- app/Exceptions/UnauthorizedException.php
+++ app/Exceptions/UnauthorizedException.php
@@ -3,8 +3,25 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Response;
 
 class UnauthorizedException extends Exception
 {
-    //
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        $response = [
+            'status' => false,
+            'message' => $this->getMessage() ?: 'Unauthorized Access',
+            'errors' => $this->getCode() ? $this->getCode() : []
+        ];
+
+        return response()->json($response, Response::HTTP_UNAUTHORIZED);
+    }
 }
